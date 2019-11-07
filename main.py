671493a58495dcedd3ee1da6e876fc16c0d88329
russellgoldman <russellgoldman1@gmail.com>
2019-11-07 12:42:33 -0500

    up

--- main.py
+++ main.py
@@ -8,24 +8,17 @@ if __name__ == '__main__':
     # Initialize
     kp = keypad()
  
-    # waiting for a keypress
-    digit = None
-    while digit == None:
-        digit = kp.getKey()
-    # Print result
-    print digit
-    time.sleep(0.5)
- 
     ###### 4 Digit wait ######
     seq = []
     for i in range(4):
         digit = None
         while digit == None:
             digit = kp.getKey()
+            print(digit)
         seq.append(digit)
         time.sleep(0.4)
  
     # Check digit code
     print(seq)
     if seq == [1, 2, 3, '#']:
-        print "Code accepted"
+        print("Code accepted")
