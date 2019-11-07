15b463378a8c44f19af5d8c5e370821e39efb78d
russellgoldman <russellgoldman1@gmail.com>
2019-11-07 12:35:52 -0500

    Initial commit

---
+++ main.py
@@ -0,0 +1,31 @@
+import time
+import RPi.GPIO as GPIO
+from keypad import keypad
+ 
+GPIO.setwarnings(False)
+ 
+if __name__ == '__main__':
+    # Initialize
+    kp = keypad()
+ 
+    # waiting for a keypress
+    digit = None
+    while digit == None:
+        digit = kp.getKey()
+    # Print result
+    print digit
+    time.sleep(0.5)
+ 
+    ###### 4 Digit wait ######
+    seq = []
+    for i in range(4):
+        digit = None
+        while digit == None:
+            digit = kp.getKey()
+        seq.append(digit)
+        time.sleep(0.4)
+ 
+    # Check digit code
+    print(seq)
+    if seq == [1, 2, 3, '#']:
+        print "Code accepted"
